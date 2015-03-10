c9fe3f6df350d5a24d1565da345ee5aa70a3ff93
serere <kerorosite.inc@gmail.com>
2015-03-10 16:38:39 +0100

    modificar GitAux.java

--- GitAux.java
+++ GitAux.java
@@ -12,13 +12,14 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+linea trampa
     }
     
     public void metodoAlumno2(){
-        System.out.println("metodo 2");
+        System.out.println("method 666");
     }
     
     public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
+
     }
 }
